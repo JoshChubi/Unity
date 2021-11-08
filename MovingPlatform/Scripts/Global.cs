2c18c81f93b38ec242803f0067c7cbedd037e235
Josh Ortiz <37316797+JoshChubi@users.noreply.github.com>
2021-11-07 23:54:09 -0800

    Create Global.cs

---
+++ MovingPlatform/Scripts/Global.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Global
+{
+    public enum Colors
+    {
+            Red,
+            Green,
+            Blue,
+            Gray,
+            Random
+    };
+
+    public enum States
+    {
+        MainMenu,
+        Configuration,
+        Program,
+        Simulation
+    };
+}
